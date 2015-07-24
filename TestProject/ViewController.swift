0efca24c97a5203faf8339f28629384f79a4bbb3
Brendan Chan <1513@cgs.act.edu.au>
2015-07-24 12:59:42 +1000

    Added comments

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -14,8 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        // debug line of code
         println("Hello World")
-        
         
     }
 
