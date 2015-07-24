3afe8c32669383f96b0e145c82e808e81a2a0a0f
Brendan Chan <1513@cgs.act.edu.au>
2015-07-24 12:51:32 +1000

    Initial commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Brendan Chan on 24/07/2015.
+//  Copyright (c) 2015 Brendan Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        println("Hello World")
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
