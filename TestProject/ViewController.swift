68e162b63cf49e0133ba4d9b609386506aa960cc
Brendan Chan <1513@cgs.act.edu.au>
2015-07-28 10:28:14 +1000

    Comment bug fixed

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
 
     func averageFeature() {
     
+        //This line of code needed a comment
         println("This is an average feature!");
     
     }
